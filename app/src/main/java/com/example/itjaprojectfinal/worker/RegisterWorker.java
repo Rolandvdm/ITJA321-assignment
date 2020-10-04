d53543a16304ccf0df7c769f3561de028d89bf32
Dian Roland <dialand.vdm@gmail.com>
2020-10-04 16:39:13 +0200

    After registering you are sent to login page

--- app/src/main/java/com/example/itjaprojectfinal/worker/RegisterWorker.java
+++ app/src/main/java/com/example/itjaprojectfinal/worker/RegisterWorker.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
-
 import com.example.itjaprojectfinal.R;
 import com.example.itjaprojectfinal.pojo.DatabaseManager;
 import com.example.itjaprojectfinal.pojo.User;
@@ -140,6 +139,14 @@ public class RegisterWorker extends AppCompatActivity implements View.OnClickLis
         textInputEditTextPassword.setText(null);
         textInputEditTextConfirmPassword.setText(null);
     }
+
+    public void onClickLoginPage(View v){
+
+        Intent intent = new Intent(activity, MainActivity.class);
+        startActivity(intent);
+
+    }
+
 
     @Override
     public void onClick(View v) {
